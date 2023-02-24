106d3e48668c67d1395dc85417343cffe081054d
Артём <hugochacasser@gmail.com>
2023-02-24 12:26:29 -0900

    Create ASP NET CORE WEB API app

--- Notes.Backend/Presentation/Notes.WebApi/Program.cs
+++ Notes.Backend/Presentation/Notes.WebApi/Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Notes.Persistence;
+using Notes.WebApi;
 
-app.MapGet("/", () => "Hello World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        using (var scope = host.Services.CreateScope())
+        {
+            var serviceProvider = scope.ServiceProvider;
+            try
+            {
+                var context = serviceProvider.GetRequiredService<NotesDbContext>();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+        host.Run();
+    }
 
-app.Run();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
